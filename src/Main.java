9b0b820fb033d5c90a2301069d117948492f5cfa
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2018-08-29 13:18:15 -0400

    Started Memory Class
     - Created an object called a link that will allow for the memory class to operate as a HashMap
     - Created basic commands in the Memory object to retrieve information

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,33 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class Main {
+import java.util.HashMap;
 
+public class Main {
+	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		while (true) {
-			// Intakes Basic Input (i.e. 4 digit hexadecimal numbers)
+			/*  Intakes Basic Input (i.e. Memory Location + 4 digit hexadecimal numbers)
+			    Example: 10 7C0A
+				10 - Memory Location
+				7C0A - Either Instruction or Data depending on the state of the machine
+			*/
 			String[] words = br.readLine().split(" ");
 			
-			int index = Integer.parseInt(words[0]);
+			int memLoc = Integer.parseInt(words[0]);
+			String cmd = words[1];
 			
 		}
 		
+		for ()
+		
 	}
-
+	
+	private static void perform (int memLoc, String cmd) {
+		
+	}
+	
 }
--- src/Memory.java
+++ src/Memory.java
@@ -1,4 +1,44 @@
+import java.util.ArrayList;
 
 public class Memory {
-
+	
+	private ArrayList<Link> memory;
+	
+	public class Link {
+		
+		int key;
+		char opcode;
+		String value;
+		
+		public Link (int key, String value) {
+			this.key = key;
+			this.value = value;
+			opcode = value.charAt(0);
+		}
+		
+		public String toString () {
+			return "Memory Location: " + key + "Value Stored: " + value;
+		}
+		
+	}
+	
+	public Memory () { 
+		memory = new ArrayList<Link>();
+	}
+	
+	public void insert (int memLoc, String cmd) {
+		memory.add(new Link(memLoc, cmd));
+	}
+	
+	public String getValFromIndex (int index) {
+		if (memory.get(index) == null) {
+			throw new NullPointerException("Link is Missing or Deleted");
+		}
+		return memory.get(index).value;
+	}
+	
+	public void removeLink (int index) {
+		Link del = memory.get(index);
+		del = null;
+	}
 }
