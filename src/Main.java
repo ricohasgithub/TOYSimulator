71d35e4105b3614b303d6220fee2df5e6ae4062c
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2018-08-29 12:52:13 -0400

    Initial Commit
     - Started classes for all of the features of the TOY computational machine

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while (true) {
+			// Intakes Basic Input (i.e. 4 digit hexadecimal numbers)
+			String[] words = br.readLine().split(" ");
+			
+			int index = Integer.parseInt(words[0]);
+			
+		}
+		
+	}
+
+}
